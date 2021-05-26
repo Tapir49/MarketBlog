540ab9c91bb18ab0e6765d55de0e0deaec1b87f6
Mark Lang <marklangvt@gmail.com>
2021-05-26 15:42:23 -0400

    server.js now navigates to homepage

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const app = express();
+const mongoose = require("mongoose");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
+
+mongoose.connect('mongodb://localhost:27017/postsDB',
+    {useNewUrlParser: true}, function () {
+        console.log("db connection successful");
+    });
+
+const blogPostSchema = {
+    state: String,
+    municipality: String,
+    address: String,
+    banner: String,
+    posted: Date,
+    type: String,
+    region: String,
+    group: String,
+    link: String
+}
+
+const BlogPost = mongoose.model('BlogPost', blogPostSchema);
+
+app.listen(3000, function () {
+    console.log("server started at 3000");
+});
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/public/homepage.html")
+})
